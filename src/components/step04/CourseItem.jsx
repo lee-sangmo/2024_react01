e02913589af3de6f8e547d2086745eec49a9a801
lee-sangmo <cochiny@gmail.com>
2024-11-19 14:53:21 +0900

    jsx_favorit_heart

--- src/components/step04/CourseItem.jsx
+++ src/components/step04/CourseItem.jsx
@@ -1,10 +1,28 @@
-function CourseItem({title, description, image}) {
+function HeartIconBtn({ chk = false }) {
+    if(chk){
+        return(
+            <button className='btn'>
+                <img className='btn_img' src='./img/heart-fill-icon.svg' />
+            </button>
+        )
+    }
+    return(
+        <button className='btn'>
+            <img className='btn_img' src='./img/heart-icon.svg' />
+        </button>
+    )
+}
+
+function CourseItem({title, description, image, isFavorite}) {
     return (
         <article className="course">
             <img className="course__img" src={image} alt='강의 이미지' />
             <div className="course__body">
                 <div className="course__title">{title}</div>
                 <div className="course__description">{description}</div>
+            </div>
+            <div className='course_icons'>
+                <HeartIconBtn chk = {isFavorite} />
             </div>
         </article>
     );
